e0b6d92bd0125c630db921b30b1058ec2dcd8476
Максим Авдеев <awotchannel@gmail.com>
2020-09-29 23:45:16 +1000

    Поправил график, теперь не ездит туда-сюда. Разделил график на 4 независимых промежутка, у каждого отрезка одинкаовая формула с разным разбросом. Во 2 промежутке рандомно высирается какая-то хуета, которую я не успел поправить под красивую синусоиду. Осталось ее доделать и можно показывать сраке.

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
@@ -56,76 +56,69 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(897, 43);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(673, 35);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Start/Stop";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(917, 122);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Location = new System.Drawing.Point(688, 99);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(919, 165);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Location = new System.Drawing.Point(689, 134);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(132, 22);
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 2;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(919, 208);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Location = new System.Drawing.Point(689, 169);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(132, 22);
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 3;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(919, 252);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Location = new System.Drawing.Point(689, 205);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(132, 22);
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 4;
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(19, 282);
-            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(14, 229);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(1469, 551);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1102, 448);
             this.chart1.TabIndex = 5;
             this.chart1.Text = "chart1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1501, 849);
+            this.ClientSize = new System.Drawing.Size(1126, 690);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load_1);
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -17,15 +17,20 @@ namespace WindowsFormsApp2
         Random rnd = new Random();
         int stepX = 0;
         int stepI = 0;
-        double x = -10000;
+        double x1 = -10000;
+        double x2 = 10000;
 
         public Form1()
         {
             InitializeComponent();
             timer1.Tick += button1_Click;
-            timer2.Tick += Step;
+            
             chart1.ChartAreas[0].AxisY.ScaleView.Zoom(-6, 6);
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoom(-7000, 7000);
+
+
         }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -40,57 +45,64 @@ namespace WindowsFormsApp2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
+                timer1.Enabled = !timer1.Enabled;
 
-            chart1.Series[0].Points.Clear();
+                chart1.Series[0].Points.Clear();
                 chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-                
-            double f = 50;
-            const int N = 10000;
-            //1 часть    
-            int rnd_step = rnd.Next(0, 10);
-            for (int i = 10; i < N; i=i+1000)
-                {
-                double value = rnd.Next(-500,500) ;
-                double y = Math.Cos(5 * i) * Math.Cos(f) + value/10000;
-                chart1.Series[0].Points.AddXY(x, y);
-                x = x + 10 + stepX; ;
-                }
-            for (int i = 10; i < N+ 10; i = i + 100)
-            {
-                double value = rnd.Next(-500, 500);
-                double y = Math.Cos(5 * i) * Math.Cos(f) + value / 10000;
 
-                if (rnd_step == 3)
+                double f = 50;
+
+
+                int rnd_step = rnd.Next(0, 4);
+                for (int x = -10000; x < -5000; x = x + 10)
                 {
-                    for (int j = 10; j < N + 100; j += 250)
+                    double value = rnd.Next(-100, 700);
+                    double y = Math.Cos(5 * x) * Math.Cos(f) + value / 1000;
+                    chart1.Series[0].Points.AddXY(x, y);
+                }
+                for (int x = -5000; x < 0; x = x + 10)
+                {
+                    double value = rnd.Next(-500, 800);
+                    double y = Math.Cos(5 * x) * Math.Cos(f) + value / 1000;
+
+                    if (rnd_step == 2 || rnd_step == 4)
                     {
-                        double value_A = rnd.Next(-100, 100);
-                        double y_A = Math.Cos(5 * j + 6) * Math.Cos(f) + value_A / 10000;
-                        chart1.Series[0].Points.AddXY(x, y_A + 5);
-                        x = x + 5 + stepX; ;
-                    }
-                    rnd_step = rnd.Next(0, 10);
-                }
-                else chart1.Series[0].Points.AddXY(x, y);
-                x = x + 50 + stepX; ;
-            }
-            // середина
+                        int randx = rnd.Next(-5000, 0);
+                        for(int i = randx; i < randx+100; i+=50)
+                        {
+                            chart1.Series[0].Points.AddXY(randx, y + 3); // надо додумать рандомное появление красивой синусоиды в промежутке с -5000 до 0 
+                            
+                        }
 
-            for (int i = 10; i < N+ 10; i = i + 100)
+                    rnd_step = rnd.Next(1, 4);
+                }
+                    else
+                    {
+
+                        chart1.Series[0].Points.AddXY(x, y);
+                    }
+                    
+
+                }
+
+
+            for (int x = 0; x < 5000 ; x = x + 10)
             {
-                double value = rnd.Next(-100, 1000);
-                double y = Math.Cos(5 * i) * Math.Cos(f) + value / 10000;
+                double value = rnd.Next(-100, 900);
+                double y = Math.Cos(5 * x) * Math.Cos(f) + value / 1000;
                 chart1.Series[0].Points.AddXY(x, y);
-                x = x + 50 + stepX; ;
             }
-            //1 часть
-            for (int i = 10000; i < N+ 10000; i += 10000)
+
+            for (int x = 5000; x <10000; x += 10)
             {
-                double value = rnd.Next(-100, 100);
-                double y = Math.Cos(5 * i) * Math.Cos(f) + value / 10000;
+                double value = rnd.Next(-1000, 1000);
+                double y = Math.Cos(5 * x) * Math.Cos(f) + value / 1000;
                 chart1.Series[0].Points.AddXY(x, y);
-                x += 100 + stepX; ;
+
+
             }
+            
 
         }
 
@@ -98,5 +110,7 @@ namespace WindowsFormsApp2
         {
 
         }
+
+
     }
 }
